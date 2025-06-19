158a0dbf8d74f150f86b23044581e6a6ef64e300
yitzchak shaish <y350380350@gmail.com>
2025-06-19 18:29:58 +0300

    Addded epartments and implementing them poorly.

---
+++ BaseDefenseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAlertSystem
+{
+    internal class BaseDefenseController
+    {
+        private SirenAlert alert = new SirenAlert();
+
+        public void DetectThreat()
+        {
+            Console.WriteLine("Alarm activation");
+            alert.Alert();
+        }
+        
+    }
+}
---
+++ SirenAlert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAlertSystem
+{
+    internal class SirenAlert
+    {
+        public void Alert()
+        {
+            Console.WriteLine("Alert");
+        }
+    }
+}
